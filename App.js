378131ef031d7d2c688a224dcad952d1f0b9400b
BalachandranKanagasundaram <bala.chandran@exelaonline.com>
2020-06-15 00:39:04 -0400

    Product screen Label was changed

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import LoginScreen from './src/screens/Login/LoginScreen';
 import HomeScreen from './src/screens/Home/HomeScreen';
 import * as ThemeColor from './src/themes/colors';
 import Product from './src/screens/Products/ProductScreen';
+import ProductScreen from './src/screens/Products/ProductScreen';
 
 class App extends Component {
   render() {
@@ -13,14 +14,24 @@ class App extends Component {
     return (
       <NavigationContainer>
         <Drawer.Navigator
-          initialRouteName="HomeScreen"
+          initialRouteName="Logout"
           drawerStyle={{backgroundColor: ThemeColor.LightDrawerColor}}
           drawerContentOptions={{
             activeBackgroundColor: ThemeColor.DarkColor,
             activeTintColor: ThemeColor.PrimaryTextColor,
           }}>
-          <Drawer.Screen name="Home" component={HomeScreen} initialParams={{ storeId: null }}/>
-          <Drawer.Screen name="Product" component={Product} />
+          <Drawer.Screen
+            name="Home"
+            component={HomeScreen}
+            initialParams={{storeId: null}}
+          />
+          <Drawer.Screen
+            name="Product"
+            component={ProductScreen}
+            initialParams={{storeId: '2', storeName: 'singhotel'}}
+          />
+          <Drawer.Screen name="Support" component={HomeScreen} />
+          <Drawer.Screen name="FAQ" component={HomeScreen} />
           <Drawer.Screen name="Logout" component={LoginScreen} />
         </Drawer.Navigator>
       </NavigationContainer>
--- src/screens/products/ProductScreen.js
+++ src/screens/products/ProductScreen.js
@@ -1,105 +1,107 @@
-import React, { Component } from 'react';
-import { SafeAreaView, FlatList } from 'react-native';
+import React, {Component} from 'react';
+import {SafeAreaView, FlatList} from 'react-native';
 import styles from './styles';
 import StatusBarComponent from '../../components/StatusBar/StatusBarComponent';
-import { Card, ListItem, Text, PricingCard } from 'react-native-elements';
+import {Card, ListItem, Text, PricingCard} from 'react-native-elements';
 import ProductAPI from '../../api/Products/ProductAPI';
 import Header from '../../components/Header/Header';
-import { getItemTransformedItemDesc } from "./utils";
-import ActivityContainer from "../../components/ActivityIndicator/ActivityContainer";
-import * as ThemeColor from "../../themes/colors";
-import { NO_PRODUCTS } from "./constants";
+import {getItemTransformedItemDesc} from './utils';
+import ActivityContainer from '../../components/ActivityIndicator/ActivityContainer';
+import * as ThemeColor from '../../themes/colors';
+import {NO_PRODUCTS} from './constants';
 
 class ProductScreen extends Component {
-    state = {
+  state = {
+    productList: [],
+    message: '',
+    isLoading: false,
+    infoMessage: NO_PRODUCTS,
+  };
+  componentDidMount = async () => {
+    this.setState({
+      isLoading: true,
+      productList: [],
+      infoMessage: '',
+    });
+    const productList = await ProductAPI.GetProductBasedonStoreId(this.props.route.params.storeId);
+    this.setState({productList}, () => {
+      this.state.productList.length === 0 &&
+        this.setState({infoMessage: NO_PRODUCTS});
+      this.setState({isLoading: false});
+    });
+  };
+  componentDidUpdate = async prevProps => {
+    if (this.props.route.params.storeId !== prevProps.route.params.storeId) {
+      this.setState({
+        isLoading: true,
         productList: [],
-        message: "",
-        isLoading: false,
-        infoMessage: NO_PRODUCTS
-    };
-    componentDidMount = async () => {
-        this.setState({
-            isLoading: true,
-            productList: [],
-            infoMessage: ""
-        })
-        const productList = await ProductAPI(this.props.route.params.storeId);
-        this.setState({ productList }, () => {
-            this.state.productList.length === 0 &&
-                this.setState({ infoMessage: NO_PRODUCTS })
-            this.setState({ isLoading: false });
-        });
+        infoMessage: '',
+      });
+      const productList = await ProductAPI.GetProductBasedonStoreId(this.props.route.params.storeId);
+      this.setState({productList}, () => {
+        this.state.productList.length === 0 &&
+          this.setState({infoMessage: NO_PRODUCTS});
+        this.setState({isLoading: false});
+      });
     }
-    componentDidUpdate = async (prevProps) => {
-        if (this.props.route.params.storeId !== prevProps.route.params.storeId) {
-            this.setState({
-                isLoading: true,
-                productList: [],
-                infoMessage: ""
-            })
-            const productList = await ProductAPI(this.props.route.params.storeId);
-            this.setState({ productList }, () => {
-                this.state.productList.length === 0 &&
-                    this.setState({ infoMessage: NO_PRODUCTS })
-                this.setState({ isLoading: false });
-            });
-        }
-    }
-    componentWillUnmount() {
-        this.setState({ infoMessage: "" })
-    }
-    renderRating = (rating) => {
-        return ""
-    }
-    render() {
-        const { storeName, storeOpen } = this.props.route.params
-        return (
-            <SafeAreaView style={styles.container}>
-                <StatusBarComponent styleType={0} />
-                <Header navigation={this.props.navigation} titleValue={storeName} />
-                <ActivityContainer isLoading={this.state.isLoading} />
-                {this.state.productList.length === 0 ?
-                    <Text h3>{this.state.infoMessage}</Text> :
-                    <FlatList
-                        data={this.state.productList}
-                        renderItem={({ item }) => (
-                            <Card
-                                containerStyle={styles.cardContainer}
-                                title={item.name.toUpperCase()}
-                                key={item.id}
-                                titleStyle={styles.cardTitle}
-                                wrapperStyle={styles.cardContentContainer}
-                            >
-                                <ListItem
-                                    key={item.id}
-                                    leftAvatar={{
-                                        source: { uri: item.images[0].src },
-                                        style: styles.productImage
-                                    }}
-                                    subtitle={<PricingCard
-                                        containerStyle={styles.pricingContainer}
-                                        titleStyle={styles.pricingTitle}
-                                        price={"Rs." + item.price}
-                                        pricingStyle={styles.priceText}
-                                        info={[
-                                            getItemTransformedItemDesc(item.short_description)
-                                        ]}
-                                        infoStyle={styles.description}
-                                        button={{ title: 'ADD', icon: 'shopping-cart' }}
-                                        color={ThemeColor.DarkColor}
-                                    />
-                                    }
-                                />
-                            </Card>
-
-                        )
-                        }
-                        keyExtractor={item => item.id}
+  };
+  componentWillUnmount() {
+    this.setState({infoMessage: ''});
+  }
+  renderRating = rating => {
+    return '';
+  };
+  render() {
+    const {storeName, storeOpen} = this.props.route.params;
+    return (
+      <SafeAreaView>
+        <StatusBarComponent styleType={0} />
+        <Header navigation={this.props.navigation} titleValue="Products" />
+        <ActivityContainer isLoading={this.state.isLoading} />
+        <Text style={{marginLeft: 20, marginTop: 20}}>
+          {storeName}
+        </Text>
+        {this.state.productList.length === 0 ? (
+          <Text h3>{this.state.infoMessage}</Text>
+        ) : (
+          <FlatList
+            data={this.state.productList}
+            renderItem={({item}) => (
+              <Card
+                containerStyle={styles.cardContainer}
+                title={item.name.toUpperCase()}
+                key={item.id}
+                titleStyle={styles.cardTitle}
+                wrapperStyle={styles.cardContentContainer}>
+                <ListItem
+                  key={item.id}
+                  leftAvatar={{
+                    source: {uri: item.images[0].src},
+                    style: styles.productImage,
+                  }}
+                  subtitle={
+                    <PricingCard
+                      containerStyle={styles.pricingContainer}
+                      titleStyle={styles.pricingTitle}
+                      price={'Rs.' + item.price}
+                      pricingStyle={styles.priceText}
+                      info={[
+                        getItemTransformedItemDesc(item.short_description),
+                      ]}
+                      infoStyle={styles.description}
+                      button={{title: 'ADD', icon: 'shopping-cart'}}
+                      color={ThemeColor.DarkColor}
                     />
-                }
-            </SafeAreaView >
-        );
-    }
+                  }
+                />
+              </Card>
+            )}
+            keyExtractor={item => item.id}
+          />
+        )}
+      </SafeAreaView>
+    );
+  }
 }
 
 export default ProductScreen;
