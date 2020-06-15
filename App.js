4520b35330fb44ab670cb8cc01b5eafeeccbcb29
sunilprakash96 <sunilprakash96@gmail.com>
2020-06-16 00:08:02 +0530

    Completed Tab Navigation

--- App.js
+++ App.js
@@ -1,39 +1,32 @@
-import React, {Component} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createDrawerNavigator, DrawerContent} from '@react-navigation/drawer';
-import {View, TouchableOpacity, Text, Image} from 'react-native';
+import React, { Component } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LoginScreen from './src/screens/Login/LoginScreen';
 import HomeScreen from './src/screens/Home/HomeScreen';
+import  { getTabIcons } from "./src/navigations/utils";
 import * as ThemeColor from './src/themes/colors';
-import Product from './src/screens/Products/ProductScreen';
-import ProductScreen from './src/screens/Products/ProductScreen';
+
 
 class App extends Component {
   render() {
-    const Drawer = createDrawerNavigator();
+    const Tab = createBottomTabNavigator();
     return (
       <NavigationContainer>
-        <Drawer.Navigator
-          initialRouteName="Logout"
-          drawerStyle={{backgroundColor: ThemeColor.LightDrawerColor}}
-          drawerContentOptions={{
-            activeBackgroundColor: ThemeColor.DarkColor,
-            activeTintColor: ThemeColor.PrimaryTextColor,
-          }}>
-          <Drawer.Screen
-            name="Home"
-            component={HomeScreen}
-            initialParams={{storeId: null}}
-          />
-          <Drawer.Screen
-            name="Product"
-            component={ProductScreen}
-            initialParams={{storeId: '2', storeName: 'singhotel'}}
-          />
-          <Drawer.Screen name="Support" component={HomeScreen} />
-          <Drawer.Screen name="FAQ" component={HomeScreen} />
-          <Drawer.Screen name="Logout" component={LoginScreen} />
-        </Drawer.Navigator>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => 
+              getTabIcons(route, focused, color, size),
+          })}
+          tabBarOptions={{
+            activeTintColor: ThemeColor.DarkColor,
+            inactiveTintColor: 'gray',
+          }}
+          >
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Search" component={HomeScreen} />
+          <Tab.Screen name="Account" component={HomeScreen} />
+          <Tab.Screen name="Cart" component={HomeScreen} />
+        </Tab.Navigator>
       </NavigationContainer>
     );
   }
