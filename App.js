a52dc0bf4de40017196ca22e9e37573dd6356057
BalachandranKanagasundaram <bala.chandran@exelaonline.com>
2020-06-18 13:26:31 -0400

    Navigation  issue fixed. Issue in adding item to cart was done.

--- App.js
+++ App.js
@@ -1,33 +1,57 @@
-import React, { Component } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React, {Component} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from './src/screens/Login/LoginScreen';
 import HomeScreen from './src/screens/Home/HomeScreen';
-import  { getTabIcons } from "./src/navigations/utils";
+import {getTabIcons} from './src/navigations/utils';
 import * as ThemeColor from './src/themes/colors';
 import ProductsScreen from './src/screens/Products/ProductsScreen';
 import CartScreen from './src/screens/Cart/CartScreen';
 
 class App extends Component {
-  render() {
+  
+  
+  
+  loginScreenNavigator = () => {
+    let Stack = createStackNavigator();
+    return (
+      <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    );
+  };
+
+  homeScreenNavigator = () => {
     const Tab = createBottomTabNavigator();
     return (
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) =>
+            getTabIcons(route, focused, color, size),
+        })}
+        tabBarOptions={{
+          activeTintColor: ThemeColor.DarkColor,
+          inactiveTintColor: 'gray',
+        }}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Search" component={ProductsScreen} />
+        <Tab.Screen name="Cart" component={CartScreen} />
+        <Tab.Screen name="Account" component={HomeScreen} />
+      </Tab.Navigator>
+    );
+  };
+
+  render() {
+    const RootStack = createStackNavigator();
+   
+    return (
       <NavigationContainer>
-        <Tab.Navigator initialRouteName="Home"
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => 
-              getTabIcons(route, focused, color, size),
-          })}
-          tabBarOptions={{
-            activeTintColor: ThemeColor.DarkColor,
-            inactiveTintColor: 'gray',
-          }}
-          >
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Search" component={ProductsScreen} />
-          <Tab.Screen name="Cart" component={CartScreen} />
-          <Tab.Screen name="Account" component={LoginScreen} />
-        </Tab.Navigator>
+        <RootStack.Navigator screenOptions={{headerShown:false}} initialRouteName="HomeScreen">
+          <RootStack.Screen name="LoginScreen" component={this.loginScreenNavigator} />
+          <RootStack.Screen name="HomeScreen" component={this.homeScreenNavigator} />
+        </RootStack.Navigator>
       </NavigationContainer>
     );
   }
