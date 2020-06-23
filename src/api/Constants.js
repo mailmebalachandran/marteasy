ee495218bfaae772237b2b759849ab8a5c2fdbeb
BalachandranKanagasundaram <bala.chandran@exelaonline.com>
2020-06-23 16:16:52 -0400

    Add to cart screen design was changed and functonality was partially done.

--- src/api/Constants.js
+++ src/api/Constants.js
@@ -2,7 +2,7 @@ export const LoginAPI = "https://marteasy.vasanthamveliyeetagam.com/wp-json/jwt-
 export const GetProductsAPI = "https://marteasy.vasanthamveliyeetagam.com/wp-json/dokan/v1/stores/";
 export const GetShopsAPI = "https://marteasy.vasanthamveliyeetagam.com/wp-json/dokan/v1/stores/";
 export const SearchProductsAPI = "https://marteasy.vasanthamveliyeetagam.com/wp-json/wc/v3/products?search=";
-
+export const CartGetProductsAPI = "https://marteasy.vasanthamveliyeetagam.com/wp-json/wc/v3/products?consumer_key=";
 
 export const CONSUMER_KEY = "ck_6dcda63598acde7f3c8f52a07095629132ca84ed";
 export const CONSUMER_SECRET = "cs_8757c7474b8093821cec8468c09a2cacb9ccb65c";
