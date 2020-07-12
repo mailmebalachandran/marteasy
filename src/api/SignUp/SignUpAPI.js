aaabf4219a8eebdb5815971ffcea9340069be2b7
sunilprakash96 <sunilprakash96@gmail.com>
2020-07-12 17:33:43 +0530

    Migrated AsyncStorage To Community

--- src/api/SignUp/SignUpAPI.js
+++ src/api/SignUp/SignUpAPI.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
-import { AsyncStorage } from 'react-native';
+import { AsyncStorage } from "@react-native-community/async-storage";
 import * as Constants from '../Constants';
 
 const SignUpValidation = async userDetails => {
